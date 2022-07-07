6a10f1e22abfa1a5ee81af0ee19dd11a7c4fd7e2
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-07 17:41:11 -0400

    added url input for faster usage + removed useless shit

--- ttreport.py
+++ ttreport.py
@@ -8,16 +8,14 @@ from tkinter.filedialog import askopenfilename
 menu = """╦═╗┌─┐┌─┐┌─┐┬─┐┌┬┐
 ╠╦╝├┤ ├─┘│ │├┬┘ │ 
 ╩╚═└─┘┴  └─┘┴└─ ┴ """
-threads = 500
-proxiesloaded = False
-proxies = []
-proxy = []
 os.system(f'title tiktok reporter ^| github/x8g')
 Anime.Fade(Center.Center(menu), Colors.blue_to_purple, Colorate.Vertical, interval=0.05, time=5)
 Anime.Fade(Center.Center("Tiktok Mass Reporter"), Colors.blue_to_purple, Colorate.Vertical, interval=0.05, time=2)
 Anime.Fade(Center.Center("By zt#7380"), Colors.blue_to_purple, Colorate.Vertical, interval=0.01, time=3)
-Anime.Fade(Center.Center("sends reports"), Colors.blue_to_purple, Colorate.Vertical, interval=0.05, time=5)
-
+Anime.Fade(Center.Center("sends reports"), Colors.blue_to_purple, Colorate.Vertical, interval=0.05, time=2)
+# ---
+threads = 50
+# ---
 f = open('proxies.txt','wb')
 proxytype = 'http'
 if proxytype not in ['http', 'https', 'socks4', 'socks5']:
@@ -26,46 +24,16 @@ if proxytype not in ['http', 'https', 'socks4', 'socks5']:
 r1 = requests.get(f"https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxytype}&timeout=10000&country=all")
 f.write(r1.content)
 f.close()
-
+# --- proxy scraping feel free to add proxy usage!
 print(Colorate.Horizontal(Colors.blue_to_purple, 'Successfully scraped proxies.'))
-
-def load_proxies():
-    global filename
-    if not os.path.exists(filename):
-        print("File Error")
-        time.sleep(10)
-        sys.exit()
-    with open(filename, "r", encoding = "UTF-8") as f:
-        for line in f.readlines():
-            line = line.replace("\n", "")
-            proxies.append(line)
-            proxiesloaded = True
-        if not len(proxies):
-            print("No proxies in file")
-            time.sleep(10)
-            sys.exit()
-
-option = int(input(Colorate.Horizontal(Colors.blue_to_purple, "(1) proxies | (2) proxyless ")))
-if option == 1:
-    if os.name == 'nt':
-        print(Colorate.Horizontal(Colors.blue_to_purple,"Please select your proxies file"))
-        Tk().withdraw()
-        filename = askopenfilename()
-        #print(filename)
-    else:
-        filename = input("Please enter where your proxies are stored. (eg etc/home/proxies.txt): ")
-    load_proxies()
-
-
+url = input(Colorate.Horizontal(Colors.blue_to_purple, 'Url?: '))
 def report():
-    global proxies
     while True:
-        r = requests.get(f'your url')
-        if r.status_code == 200:
-            print(Fore.MAGENTA, r.text)
-            print(Colorate.Horizontal(Colors.green_to_red, 'reported that fag!'))
+        r = requests.get(url)
+        if "Thanks for your feedback" in r.text:
+            print(Colorate.Horizontal(Colors.green_to_red, f'reported that fag!'))
         else:
-            print(Colorate.Horizontal(Colors.red_to_white, 'User couldn\'t be reported due to rate limitations'))
+            print(Colorate.Horizontal(Colors.red_to_white, f'User couldn\'t be reported due to rate limitations'))
 while True:
         if threading.active_count() < int(threads):
             threading.Thread(target=report).start() 
